084ef8bf2c9a601c64edc3665aa86c57bdfa5a4c
francisco gonzalez <franciscogonzalezhontoria@gmail.com>
2024-08-19 13:53:15 +0200

    new class for user dto

---
+++ src/main/java/com/course/springboot/webapp/models/dto/UserDto.java
@@ -0,0 +1,25 @@
+package com.course.springboot.webapp.models.dto;
+
+import com.course.springboot.webapp.models.User;
+
+public class UserDto {
+
+    private String title;
+    private User user;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+}
